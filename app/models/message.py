c8904695bfcc938a233819c1377346585bba54f9
TheSmuks <therealsmuks@gmail.com>
2025-10-06 17:40:02 +0200

    chore[#20]: add base message model

---
+++ app/models/message.py
@@ -0,0 +1,46 @@
+import uuid
+from datetime import datetime
+from typing import Optional
+
+import sqlalchemy as sa
+from pydantic import BaseModel, Field
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+from app.models.user import User
+
+Base = declarative_base()
+
+
+class Message(BaseModel):
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    content: str = Field(min_length=1, max_length=500)
+    channel_id: str
+    user_id: str
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: Optional[datetime] = None
+    is_edited: bool = Field(default=False)
+    parent_message_id: Optional[str] = None
+
+    user: Optional[User] = None
+
+    class Config:
+        from_attributes = True
+
+
+class MessageTableModel(Base):
+    __tablename__ = "message"
+
+    id = Column(sa.UUID, primary_key=True, default=uuid.uuid4)
+    content = Column(sa.Text(500), nullable=False)
+    channel_id = Column(sa.UUID, ForeignKey("channel.id"), nullable=False)
+    user_id = Column(sa.UUID, ForeignKey("user.id"), nullable=False)
+    created_at = Column(sa.DateTime, nullable=False, default=datetime.now)
+    updated_at = Column(sa.DateTime, nullable=True)
+    is_edited = Column(sa.Boolean, nullable=False, default=False)
+    parent_message_id = Column(sa.UUID, ForeignKey("message.id"), nullable=True)
+
+    user = relationship("User", back_populates="messages")
+    channel = relationship("ChannelTableModel", back_populates="messages")
+
+    parent_message = relationship("MessageTableModel", remote_side=[id], backref="replies")
