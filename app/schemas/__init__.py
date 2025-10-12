746e719dab6a1c7542d4c365085d1290330f19b5
TheSmuks <therealsmuks@gmail.com>
2025-10-12 21:09:35 +0200

    ci: fix deploy workflow

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,6 +1,7 @@
-from app.schemas.auth import AuthResponse, LoginRequest
 from app.schemas.channel import ChannelBase, ChannelCreate, ChannelRead, ChannelUpdate, MembershipRead
 from app.schemas.user import UserInDB, UserPublic
+
+from app.schemas.auth import AuthResponse, LoginRequest  # isort: skip
 
 ChannelRead.model_rebuild()
 ChannelCreate.model_rebuild()
@@ -10,7 +11,6 @@ UserPublic.model_rebuild()
 UserInDB.model_rebuild()
 AuthResponse.model_rebuild()
 LoginRequest.model_rebuild()
-UserPublic.model_rebuild()
 
 __all__ = [
     "ChannelRead",
@@ -22,5 +22,4 @@ __all__ = [
     "UserInDB",
     "AuthResponse",
     "LoginRequest",
-    "UserPublic",
 ]
