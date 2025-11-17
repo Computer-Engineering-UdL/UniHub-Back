5700a4e519c9a34f119435878dbe58e7d13f5d35
Jerzy Legaszewski <91875468+JerzyLeg@users.noreply.github.com>
2025-11-17 20:26:19 +0100

    fix[263]: refactor public signup, add faculty_id. Add university endpoints to list universities and list universities & faculties

--- app/api/v1/endpoints/university.py
+++ app/api/v1/endpoints/university.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 
 from app.core.database import get_db
 from app.domains.university.university_service import UniversityService
-from app.schemas.university import UniversityRead
+from app.schemas.university import UniversityRead, UniversitySimpleRead
 
 router = APIRouter()
 
@@ -14,11 +14,17 @@ def get_university_service(db: Session = Depends(get_db)) -> UniversityService:
     """Dependency to inject HousingCategoryService."""
     return UniversityService(db)
 
-
 @router.get("/", response_model=List[UniversityRead])
-def get_universities(university_service=Depends(get_university_service)):
+def get_universities(university_service: UniversityService = Depends(get_university_service)):
     """
     Gets a list of all universities and their faculties
     to populate the frontend dropdowns.
     """
-    return get_university_service.get_all_with_faculties()
+    return university_service.get_all_with_faculties()
+
+@router.get("/simple", response_model=List[UniversitySimpleRead])
+def get_universities_simple(university_service: UniversityService = Depends(get_university_service)):
+    """
+    Returns universities without faculties.
+    """
+    return university_service.get_all_simple()
